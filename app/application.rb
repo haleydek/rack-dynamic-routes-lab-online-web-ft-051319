074d81c6c1d6a3db6126e8e39f91797cbe0a09dd
Haley DeKeyser <hcd09@live.com>
2019-06-20 20:52:38 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split('/items/').last
-      item = @@items.select { |item| item.name == item_name }
+      item = @@items.select { |item| item.name == item_name }.flatten
       if item == nil
         resp.write "Route not found"
         resp.status = 400
