7c364891253e02ece49a2fcfd2916f8a2c1c83cf
Haley DeKeyser <hcd09@live.com>
2019-06-20 20:23:55 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,24 @@
+class Application
+  
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    
+    if req.path.match(/items/)
+      item_name = req.path.split('/items/').last
+      item = Item.all.find? { |item| item.name == item_name }
+      
+      if item == nil?
+        #status code = 404
+      else
+        resp.write "#{item.price}"
+      end
+      
+    else
+      #status code = 404
+    end
+    
+    resp.finish
+  end
+  
+end
