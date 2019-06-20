c5876753129aec1ce54e6ceaccf1ec14865c8ba5
Haley DeKeyser <hcd09@live.com>
2019-06-20 20:44:28 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split('/items/').last
-      if @@items.include?(item_name)
+      if @@items.find { |item| item.name == item_name }
         resp.write "#{item.price}"
       else
         resp.write "Route not found"
